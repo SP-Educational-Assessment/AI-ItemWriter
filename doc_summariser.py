8a43418058e7c785794272ffc541224c2134ba79
James Page <james@ecreationtech.co.uk>
2023-05-15 21:47:07 +0100

    Properly linted

--- doc_summariser.py
+++ doc_summariser.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python
+"""Summarises a Word doc for a child to be no less than 1500 words."""
+
+#!/usr/bin/env python3
 # coding: utf-8
 
-# # Word Doc Summariser
-# 
-# Summarises a Word doc for a child to be no less than 1500 words
-
 import os
-import openai
-from dotenv import load_dotenv
-from docx import Document
 import re
-import spacy
 
-MIN_PARA_CHARS=300
+import openai
+import spacy
+from docx import Document
+from dotenv import load_dotenv
+
+MIN_PARA_CHARS = 300
 
 load_dotenv()
 
@@ -20,61 +19,101 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORG")
 
 nlp = spacy.load("en_core_web_sm")
-stopMarks = set('?!.')
+stopMarks = set("?!.")
 
 
-def returnFullSentences(text):
+def return_full_sentences(text):
+    """
+    Tokenizes the input text into sentences and returns only those that end
+    with certain punctuation marks.
+
+    Args:
+    text (str): The input text to be tokenized.
+
+    Returns:
+    str: A string containing all the full sentences found in the input text,
+    joined together with a single space.
+
+    Raises:
+    ValueError: If the input text is empty or None.
+    """
+
     doc = nlp(text)
     sentences = [s.text for s in doc.sents if s.text[-1:] in stopMarks]
-    return ' '.join(sentences)
+    return " ".join(sentences)
 
 
-def summarise4Child(text):
-    summarisationPrompt = "Summarize this paragraph for a ten year old child:\n\n"
-    fullPrompt = ''.join([summarisationPrompt, text])
-    numWords = len(re.findall(r'\w+', text))
-    tokens = numWords // 2          # output summary should be no more than half the original text length
+def summarise_4_child(text):
+    """
+    Summarises the text input for a 10 year old child.
+
+    Args:
+    text (str): The input text to be summarised.
+
+    Returns:
+    str: A string containing the summary.
+
+    Raises:
+    ValueError: If the input text is empty or None.
+    """
+
+    summarisation_prompt = "Summarize this paragraph for a ten year old child:\n\n"
+    full_prompt = "".join([summarisation_prompt, text])
+    num_words = len(re.findall(r"\w+", text))
+    tokens = (
+        num_words // 2
+    )  # output summary should be no more than half the original text length
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=fullPrompt,
+        prompt=full_prompt,
         temperature=0.4,
         max_tokens=tokens,
         top_p=1.0,
         frequency_penalty=0.0,
-        presence_penalty=0.0)
-    
-    firstChoice = response.choices[0]
-    summary = returnFullSentences(firstChoice['text'])
-    
+        presence_penalty=0.0,
+    )
+
+    first_choice = response.choices[0]
+    summary = return_full_sentences(first_choice["text"])
+
     return summary
 
 
 def main():
-    fileName = 'data/ChatGPT.docx'
-    document = Document(fileName)
-    summaryDoc = []
-    totalWords = 0
+    """
+    The main function that summarizes the text content of a given Word document file.
+
+    Returns:
+    None
+
+    Raises:
+    IOError: If the input Word document file cannot be found or opened.
+    """
+
+    file_name = "data/ChatGPT.docx"
+    document = Document(file_name)
+    summary_doc = []
+    total_words = 0
     p = 1
 
     for para in document.paragraphs:
         text = para.text
         if len(text) > MIN_PARA_CHARS:
-            textWords = len(re.findall(r'\w+', text))
-            summary = summarise4Child(text)
-            summaryWords = len(re.findall(r'\w+', summary))
-            totalWords += summaryWords
-            summaryDoc.append(summary)
-            print("P{}: IN WORDS = {}, OUT WORDS = {}".format(p, textWords, summaryWords))
+            text_words = len(re.findall(r"\w+", text))
+            summary = summarise_4_child(text)
+            summary_words = len(re.findall(r"\w+", summary))
+            total_words += summary_words
+            summary_doc.append(summary)
+            print(f"P{p}: IN WORDS = {text_words}, OUT WORDS = {summary_words}")
             p += 1
 
     # now write out to a new text document
-    with open('summary.txt', 'w') as f:
-        for s in summaryDoc:
+    with open("summary.txt", "w", encoding="utf-8") as f:
+        for s in summary_doc:
             f.write(s)
 
-    print("Total of {} words".format(totalWords))
-
+    print(f"Total of {total_words} words")
 
 
 if __name__ == "__main__":
